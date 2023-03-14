ff2d691740781adaf163d14e68612e8ed5215985
Angelina <gela-turlova@inbox.ru>
2023-03-14 23:43:38 +0300

    added the appearance of a tasks by clicking 'plus'

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 'use strict'
 
+window.onload = init;
 
-add.onclick = function() {
-    let addTask = document.getElementById('t1');
-    addTask.innerHTML = 'may may may may';
+function init() {
+    const add = document.getElementById('add');
+    add.onclick = function() {
+        createTask();
+    }
 };
 
-window.onload = init;
+function createTask() {
+    let conteiner = document.getElementById('conteinerTask');
+    let newTask = document.createElement('div');
+    let text = document.createElement('div');
+    let buttonDone = document.createElement('div');
+    let buttonDelete = document.createElement('div');
+    newTask.classList.add('task');
+    text.classList.add('textStyle');
+    text.innerHTML = document.getElementById('newtask').value;
+    buttonDone.innerHTML = '<button type="button" id="done"><img class="doneImg" src="./img/check.svg" alt="done"></button>';
+    buttonDelete.innerHTML = '<button type="button" id="delete" ><img class="deleteImg" src="./img/trash.svg" alt="delete"></button>'; 
+    conteiner.appendChild(newTask);
+    newTask.appendChild(text);
+    newTask.appendChild(buttonDone);
+    newTask.appendChild(buttonDelete);
+}

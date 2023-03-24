ade8ace89e0ba42a0042144f621b6cd73e960221
Angelina <gela-turlova@inbox.ru>
2023-03-24 23:31:09 +0300

    added ability to restore todolist after page reload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
 'use strict'
 
+const tasks = getTasks('savedTasks') || [];
+
 window.onload = init;
 
 function init() {
+    console.log('start of init');
     const add = document.getElementById('add');
     let select = document.getElementById('sel');
 
     let selectedOption = 'active';
 
-    select.onchange = function() {
+    select.onchange = function () {
         selectedOption = getSelectedOption(select.selectedIndex);
 
         changeSelectByState(selectedOption);
     };
 
-    add.onclick = function() {
+    add.onclick = function () {
         if (selectedOption === 'active') {
-            createTask();
+            createTask(document.getElementById('newtask').value, false);
         }
+        document.getElementById("newtask").value = "";      // очистить значение поля ввода
     };
-}
 
-function removeTask(e) {
-    e.target.remove();
+    for (const task of tasks) {
+        createTask(task, true);
+    }
 }
 
 function getSelectedOption(index) {
@@ -46,7 +50,7 @@ function changeSelectByState(state) {
     conteinerDeleted.classList[state === 'deleted' ? 'remove' : 'add']('invisible');
 }
 
-function createTask() {
+function createTask(task, isInit) {
     const conteinerActive = document.getElementById('activeTaskConteiner');
     const conteinerCompleted = document.getElementById('completedTaskConteiner');
     const conteinerDeleted = document.getElementById('deletedTaskConteiner');
@@ -56,7 +60,7 @@ function createTask() {
     const buttonDelete = document.createElement('div');
     newTask.classList.add('task');
     text.classList.add('textStyle');
-    text.innerHTML = document.getElementById('newtask').value;
+    text.innerHTML = task.caption || task;
     buttonDone.innerHTML = '<button type="button" class="done"><img class="doneImg" src="./img/check.svg" alt="done"></button>';
     buttonDelete.innerHTML = '<button type="button" class=delete""><img class="deleteImg" src="./img/trash.svg" alt="delete"></button>';
 
@@ -64,23 +68,59 @@ function createTask() {
     newTask.appendChild(buttonDone);
     newTask.appendChild(buttonDelete);
 
-    conteinerActive.appendChild(newTask);
+    if (!isInit) {
+        tasks.push({
+            caption: task,
+            state: 'active',
+        });
+        saveTasks(tasks);
+    }
 
-    buttonDone.onclick = function() {
+    switch (task.state) {
+        case 'completed':
+            conteinerCompleted.appendChild(newTask);
+            break;
+        case 'deleted':
+            conteinerDeleted.appendChild(newTask);
+            break;
+        default:
+            conteinerActive.appendChild(newTask);
+    }
+
+
+    buttonDone.onclick = function () {
         conteinerActive.removeChild(newTask);
         conteinerCompleted.appendChild(newTask);
         newTask.style.backgroundColor = '#E8FFC4';
         newTask.style.border = '5px solid #79A630';
         buttonDone.classList.add('buttonDone');
         buttonDelete.classList.add('buttonDelete');
+
+        const foundTask = tasks.find((t) => t.caption === (task.caption || task));
+        foundTask.state = 'completed';
+        saveTasks(tasks);
     };
 
-    buttonDelete.onclick = function() {
+    buttonDelete.onclick = function () {
         conteinerActive.removeChild(newTask);
         conteinerDeleted.appendChild(newTask);
         newTask.style.backgroundColor = '#FFC4E1';
         newTask.style.border = '5px solid #A6306A';
         buttonDone.classList.add('buttonDone');
         buttonDelete.classList.add('buttonDelete');
+
+        const foundTask = tasks.find((t) => t.caption === (task.caption || task));
+        foundTask.state = 'deleted';
+        saveTasks(tasks);
     }
 }
+
+function saveTasks(tasks) {
+    localStorage.setItem('savedTasks', JSON.stringify(tasks));
+}
+
+function getTasks(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+console.log('end of script');
